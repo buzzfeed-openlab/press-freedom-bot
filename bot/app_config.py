409c576dd67bbd1bf6342b2cf0c7a064ea3f6469
Cathy Deng <cathyd125@gmail.com>
2017-07-06 16:35:53 -0700

    change config setup

--- bot/app_config.py
+++ bot/app_config.py
@@ -1,32 +1,54 @@
+import json
 import os
 
+DEFAULT_SETTINGS = {
+    "ADMIN_USER" : "admin",
+    "ADMIN_PASS" : "something-secret",
+    "DB_USER" : "root",
+    "DB_PW" : "",
+    "DB_HOST" : "localhost",
+    "DB_NAME" : "pfb",
+    "TWILIO_ACCOUNT_SID" : "",
+    "TWILIO_AUTH_TOKEN" : "",
+    "TWILIO_PHONE_NO" : "", # "+12223456789"
+    "SECRET_KEY" : "another-secret-thing",
+    "GDOC_ID" : ""
+}
 
-# TODO: tighten this up
 
-# if app_config_secret.py exists, use that to set config variables
-if os.path.isfile(os.path.dirname(__file__)+'/app_config_secret.py'):
-    from .app_config_secret import ADMIN_USER, ADMIN_PASS, SECRET_KEY, \
-                                    DB_USER, DB_PW, DB_HOST, DB_NAME, \
-                                    TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NO, \
-                                    GDOC_ID
 
-# otherwise, set config variables from environment variables,
-# & assigns them to defaults if env vars don't exist
-# (this is for deploying w/ docker)
-else:
+config_filepath = os.path.dirname(__file__)+'/app_config_secret.json'
 
-    ADMIN_USER = os.getenv('ADMIN_USER', 'admin')
-    ADMIN_PASS = os.getenv('ADMIN_PASS', 'something-secret')
 
-    SECRET_KEY = os.getenv('SECRET_KEY', 'another-secret-thing')
+# make a config file if it doesnt exist, based on default values & env vars
+if not os.path.isfile(config_filepath):
+    # TODO: a more elegant way of doing this?
 
-    DB_USER = os.getenv('DB_USER', 'root')
-    DB_PW = os.getenv('DB_PW', '')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_NAME = os.getenv('DB_NAME', 'pfb')
+    config_settings = {}
+    for k, default_v in DEFAULT_SETTINGS.items():
+        # if env var exists, use env var value - otherwise use default value
+        config_settings[k] = os.getenv(k, default_v)
 
-    TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID', '')
-    TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN', '')
-    TWILIO_PHONE_NO = os.getenv('TWILIO_PHONE_NO', '')
+    with open(config_filepath, 'w') as f:
+        json.dump(config_settings, f, indent=4)
 
-    GDOC_ID = os.getenv('GDOC_ID', '')
+
+# TODO: handle when config file exists but env vars have been updated
+
+
+# read settings from config file
+with open(config_filepath) as f:
+    json_config = json.load(f)
+
+    # TODO: a more elegant way of doing this?
+    ADMIN_USER = json_config.get('ADMIN_USER', DEFAULT_SETTINGS['ADMIN_USER'])
+    ADMIN_PASS = json_config.get('ADMIN_PASS', DEFAULT_SETTINGS['ADMIN_PASS'])
+    DB_USER = json_config.get('DB_USER', DEFAULT_SETTINGS['DB_USER'])
+    DB_PW = json_config.get('DB_PW', DEFAULT_SETTINGS['DB_PW'])
+    DB_HOST = json_config.get('DB_HOST', DEFAULT_SETTINGS['DB_HOST'])
+    DB_NAME = json_config.get('DB_NAME', DEFAULT_SETTINGS['DB_NAME'])
+    TWILIO_ACCOUNT_SID = json_config.get('TWILIO_ACCOUNT_SID', DEFAULT_SETTINGS['TWILIO_ACCOUNT_SID'])
+    TWILIO_AUTH_TOKEN = json_config.get('TWILIO_AUTH_TOKEN', DEFAULT_SETTINGS['TWILIO_AUTH_TOKEN'])
+    TWILIO_PHONE_NO = json_config.get('TWILIO_PHONE_NO', DEFAULT_SETTINGS['TWILIO_PHONE_NO'])
+    SECRET_KEY = json_config.get('SECRET_KEY', DEFAULT_SETTINGS['SECRET_KEY'])
+    GDOC_ID = json_config.get('GDOC_ID', DEFAULT_SETTINGS['GDOC_ID'])
